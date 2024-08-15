054b93d82972932c9e2206f29f908c76b1048684
RodionKirillov <rodion.44454@gmail.com>
2024-08-15 19:05:24 +0300

    Tech-15

---
+++ app/src/main/java/com/example/cryptolist/search/presentation/CryptoUsdListFragment.kt
@@ -0,0 +1,38 @@
+package com.example.cryptolist.search.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.cryptolist.databinding.FragmentCryptoRubListBinding
+import com.example.cryptolist.databinding.FragmentCryptoUsdListBinding
+
+class CryptoUsdListFragment: Fragment() {
+
+    private var _binding: FragmentCryptoUsdListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCryptoUsdListBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+
+        fun newInstance() = CryptoUsdListFragment()
+    }
+}
